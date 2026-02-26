e3093c6e7974de675668ccf3e3a79819ab492890
vtu26461-s <vtu26461@veltech.edu.in>
2026-02-26 11:02:05 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (72.08%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -1,15 +1,16 @@
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int val) { this.val = val; }
+}
+
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null)
-            return true;
-        
-        if (p == null || q == null)
-            return false;
-        
-        if (p.val != q.val)
-            return false;
-        
-        return isSameTree(p.left, q.left) &&
-               isSameTree(p.right, q.right);
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
