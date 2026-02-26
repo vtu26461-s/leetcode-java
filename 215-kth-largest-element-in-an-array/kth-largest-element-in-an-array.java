d6e0959f95f03d8846bca30a3cca10b0a6220e87
vtu26461-s <vtu26461@veltech.edu.in>
2026-02-26 11:10:14 +0530

    Time: 67 ms (54.34%) | Memory: 68.7 MB (57.05%) - LeetSync

---
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -0,0 +1,12 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        for (int num : nums) {
+            heap.add(num);
+            if (heap.size() > k) heap.poll();
+        }
+        return heap.peek();
+    }
+}
