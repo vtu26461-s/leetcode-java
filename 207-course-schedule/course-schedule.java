45aae8cdb2b3e2085ade3ef6d4d022f4acfa7c70
vtu26461-s <vtu26461@veltech.edu.in>
2026-04-21 11:28:31 +0530

    Time: 7 ms (60.23%) | Memory: 46.8 MB (88.33%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> graph = new ArrayList<>();
+        int[] indegree = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int[] p : prerequisites) {
+            graph.get(p[1]).add(p[0]);
+            indegree[p[0]]++;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.offer(i);
+            }
+        }
+
+        int count = 0;
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            count++;
+
+            for (int next : graph.get(cur)) {
+                indegree[next]--;
+                if (indegree[next] == 0) {
+                    q.offer(next);
+                }
+            }
+        }
+
+        return count == numCourses;
+    }
+}
